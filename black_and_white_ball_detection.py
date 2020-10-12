f0ff7c0476bd8afefb32c9585be1c98215caffd3
noranabdelsalam <noran130897@gmail.com>
2020-10-12 20:19:06 +0200

    test

--- black_and_white_ball_detection.py
+++ black_and_white_ball_detection.py
@@ -5,6 +5,9 @@ import imutils
 import math
 
 
+fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+out = cv2.VideoWriter('balck_and_white_ball_tracking.avi', fourcc, 30.0, (640,480))
+
 classifier = 'classifier/cascade.xml'
 
 ball_tracker = cv2.CascadeClassifier(classifier)
@@ -16,9 +19,6 @@ time.sleep(2.0)
 while True:
 	frame = vs.read()
 
-
-	frame = imutils.resize(frame, width=600)
-
 	gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	balls = ball_tracker.detectMultiScale(gray_frame,1.1,9)
@@ -29,12 +29,12 @@ while True:
 		cv2.circle(frame, (math.ceil(x+w*0.5), math.ceil(y+h*0.5)), math.ceil(w*0.5),
 			(255, 0, 255), 2)
 		
-	
+	out.write(frame)
 	cv2.imshow('Detect Ball',frame)
 	key = cv2.waitKey(1) & 0xFF
 	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
 		break
 
-
+out.release()
 cv2.destroyAllWindows()		
